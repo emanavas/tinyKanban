043ad055655f3c6d0ced29dbbe9e622966e217af
emanavas <emanavas@hotmail.com>
2025-10-16 21:58:08 +0200

    deploy settings

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.json());
 app.use(express.static(__dirname));
